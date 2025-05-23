bc3665bf4f99b94fea788bea12bde57d19f33919
Miyako Matsui <miyakomatsui18@gmail.com>
2025-05-23 18:27:24 +0900

    app is working but tailwind is not working

---
+++ src/GhibliApp.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react'
+import './index.css' 
+
+function GhibliApp() {
+    const [films, setFilms] = useState([])
+    const [selectedFilm, setSelectedFilm] = useState(null)
+
+    useEffect(() => {
+        fetch('https://ghibliapi.vercel.app/films') // ✅ 正しいエンドポイント
+            .then((res) => res.json())
+            .then((data) => setFilms(data))
+            .catch((err) => console.error(err))
+    }, [])
+
+    const showRandomFilm = () => {
+        const randomIndex = Math.floor(Math.random() * films.length)
+        setSelectedFilm(films[randomIndex])
+    }
+
+    return (
+        <div style={{ textAlign: 'center', padding: '2rem' }}>
+            <h1 class="text-red-600 ">ジブリ気分チェッカー</h1>
+            <button onClick={showRandomFilm}>今日の気分は？</button>
+
+            {selectedFilm && (
+                <div style={{ marginTop: 20 }}>
+                    <h2>
+                        {selectedFilm.title} ({selectedFilm.release_date})
+                    </h2>
+                    <p>{selectedFilm.description}</p>
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default GhibliApp
